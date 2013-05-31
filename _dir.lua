da2c5b035a58a0dc956d2f08a8f614ddbb308911
cd rubin <github@earthling.za.net>
2013-05-31 08:59:26 -0300

    Update _dir.lua

--- _dir.lua
+++ _dir.lua
@@ -1,7 +1,10 @@
 -- add some convenience and website-related functions
 pashlicks.run_file( '_lib/extensions.lua', _ENV )
 
+-- set a layout for this site
 page.layout = '_layouts/default.html'
+
+-- assign a value that will be available to all pages of the site
 page.test_var = 'test value'
 
 
